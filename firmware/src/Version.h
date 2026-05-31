ab1075dd86a045cebc17196614d90a93f2e39d63
Justin Yan <justin@justinyan.me>
2026-06-01 00:20:40 +0800

    chore(version): bump version number

--- firmware/src/Version.h
+++ firmware/src/Version.h
@@ -2,6 +2,6 @@
 namespace stopwatch {
 
 // Firmware release version (semver). Bump via firmware/tools/bump_version.py.
-inline constexpr char kFirmwareVersion[] = "0.1.3";
+inline constexpr char kFirmwareVersion[] = "0.1.4";
 
 }  // namespace stopwatch
