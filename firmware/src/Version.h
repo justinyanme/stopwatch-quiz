c87a4a5bace92fdc329bc2ac6c2d2c45ed912512
Justin Yan <justin@justinyan.me>
2026-06-01 19:20:45 +0800

    firmware: bump patch version to 0.2.1

--- firmware/src/Version.h
+++ firmware/src/Version.h
@@ -2,6 +2,6 @@
 namespace stopwatch {
 
 // Firmware release version (semver). Bump via firmware/tools/bump_version.py.
-inline constexpr char kFirmwareVersion[] = "0.2.0";
+inline constexpr char kFirmwareVersion[] = "0.2.1";
 
 }  // namespace stopwatch
